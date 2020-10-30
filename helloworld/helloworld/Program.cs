aab09e08333634fb73b9c318151308b71de43a67
yasen <xiaoqi.zhao@volvo.com>
2020-10-30 16:31:21 -0400

    add comments in the program

--- helloworld/helloworld/Program.cs
+++ helloworld/helloworld/Program.cs
@@ -4,9 +4,9 @@ namespace helloworld
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args)         //程序的Main方法
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! (from Yasen)");  //输出 "Hello World! "
             Console.ReadLine();
         }
     }

427ce14cb47f7afa894168ec145963d8ff85e209
yasen <xiaoqi.zhao@volvo.com>
2020-11-02 18:18:23 -0500

    cube as Method

---
+++ newTerminal/newTerminal/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace newTerminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(cube(5));
+        }
+
+        static int cube(int num)
+        {
+            int result = num * num * num;
+            return result;
+        }
+    }
+}

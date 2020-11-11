a4f0349cf872d8a988c199f7ac83c6377408045e
yasen <xiaoqi.zhao@volvo.com>
2020-11-11 13:43:30 -0500

    update

---
+++ dynamic_array/dynamic_array/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace dynamic_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("请输入动态数组的行数：");
+            int row = Convert.ToInt32(Console.ReadLine());
+            Console.Write("请输入动态数组的列数：");
+            int col = Convert.ToInt32(Console.ReadLine());
+
+            int[,] arr = new int[row, col];
+
+            Console.WriteLine("结果：");
+
+            for (int i = 0; i < col; i++)
+            {
+                for (int j = 0; j < row; j++)
+                {
+                    //Console.Write(i.ToString() + j.ToString() + " ");
+                    Console.Write(i+j.ToString() + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ isLeapYear/isLeapYear/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace isLeapYear
+{
+    class Program
+    {
+        //判断输入的年份是否是闰年
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入一个年份：");
+            string str = Console.ReadLine();
+
+            int year = Int32.Parse(str);
+
+            bool isleapyear = ((year % 400) == 0) || (((year % 4) == 0) && ((year % 100) != 0));
+
+            string yesno = isleapyear ? "是" : "不是";
+
+            Console.WriteLine("{0}年{1}闰年", year, yesno);
+
+            Console.ReadLine();
+        }
+    }
+}
